f52331969d6d6fdb259630b4595d49e30099409e
Abnerjm27 <abnerjoan27@gmail.com>
2026-07-09 09:41:08 -0500

    .

--- Escenarios_nivel/nivel19/hud.gd
+++ Escenarios_nivel/nivel19/hud.gd
@@ -4,3 +4,4 @@ extends CanvasLayer
 
 func actualizar_monedas(recogidas: int, total: int):
 	label_monedas.text = "Monedas: %d/%d" % [recogidas, total]
+	$Minimapa/SubViewportContainer/SubViewport.world_2d = get_viewport().world_2d
